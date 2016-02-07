ec9a57fe04a44dadd8fe0178ce8031f5f6db21e4
Omari Mayers-Walker <omari@fastmail.fm>
2016-02-07 13:43:35 -0500

    fixed typo in Home.js

--- app/components/Home.js
+++ app/components/Home.js
@@ -4,7 +4,7 @@ import React from 'react';
 import {Link} from 'react-router';
 import HomeStore from '../stores/HomeStore';
 import HomeActions from '../actions/HomeActions';
-import {first, without, findWhere} from 'understore';
+import {first, without, findWhere} from 'underscore';
 
 class Home extends React.Component {
 
